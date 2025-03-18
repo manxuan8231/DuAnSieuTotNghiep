30cf0ca1721880d543ba5bef0c4dd85bcee7522b
baovirus <thaibaoo2105@gmail.com>
2025-03-19 01:50:01 +0700

    main menu
    a

---
+++ Assets/ResourceBao/Script/CloseUI.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloseUI : MonoBehaviour
+{
+
+    public GameObject settingsPanel; // Kéo Panel vào trong Inspector
+
+    public void ToggleSettings()
+    {
+        settingsPanel.SetActive(!settingsPanel.activeSelf);
+    }
+
+    public void CloseSettings()
+    {
+        settingsPanel.SetActive(false);
+    }
+}
---
+++ Assets/ResourceBao/Script/SettingOpen.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SettingOpen : MonoBehaviour
+{
+    public GameObject settingsPanel; // Kéo Panel vào trong Inspector
+
+    public void ToggleSettings()
+    {
+        settingsPanel.SetActive(!settingsPanel.activeSelf);
+    }
+
+    public void CloseSettings()
+    {
+        settingsPanel.SetActive(false);
+    }
+}

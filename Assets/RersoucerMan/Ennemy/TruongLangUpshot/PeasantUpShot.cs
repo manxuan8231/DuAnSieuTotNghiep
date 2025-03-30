fc7bc0705abc33f401f32c8f9ad73a4af865538c
manxuan8231 <lydangxuanman05@gmail.com>
2025-03-30 23:18:18 +0700

    c

---
+++ Assets/RersoucerMan/Ennemy/TruongLangUpshot/PeasantUpShot.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PeasantUpShot : MonoBehaviour
+{
+    public Transform enemy; // Gán enemy vào Inspector
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            animator.SetTrigger("Attack");
+            PlayerUpshot playerUpshot = other.GetComponent<PlayerUpshot>(); // Tìm trực tiếp trên Player
+
+            if (playerUpshot != null)
+            {
+                playerUpshot.PlayerUpShot(enemy);
+            }
+            else
+            {
+                Debug.LogWarning("Không tìm thấy PlayerUpshot trên Player!");
+            }
+        }
+    }
+
+}
---
+++ Assets/RersoucerMan/Script/PlayerUpshot.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class PlayerUpshot : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void PlayerUpShot(Transform enemy)
+    {
+        if (enemy == null) return;
+
+        // Xác định hướng đến enemy
+        Vector3 directionToEnemy = enemy.position - transform.position;
+        directionToEnemy.y = 0; // Giữ y = 0 để tránh nhân vật nghiêng
+
+        // Gọi Flip để quay về phía enemy
+        Flip(directionToEnemy);
+    }
+
+    private void Flip(Vector3 direction)
+    {
+        // Xoay nhân vật về phía enemy
+        Quaternion newRotation = Quaternion.LookRotation(direction);
+        transform.rotation = newRotation;
+
+        // Gọi animation
+        animator.SetTrigger("HitDown");
+    }
+}

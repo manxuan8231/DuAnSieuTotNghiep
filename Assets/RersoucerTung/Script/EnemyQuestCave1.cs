a9519aa8e213f4ee1bf9e4495a3e9842fffdbd4f
Minhtung0510 <169137996+Minhtung0510@users.noreply.github.com>
2025-04-01 00:24:05 +0700

    w
    w

---
+++ Assets/RersoucerTung/Script/EnemyQuestCave1.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyQuestCave1 : MonoBehaviour
+{
+    public Transform player;  // Tham chiếu đến Player
+    private NavMeshAgent agent;
+    private bool isChasing = false; // Enemy có đang đuổi theo Player không?
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (isChasing && player != null)
+        {
+            agent.speed = 10;
+            agent.SetDestination(player.position); // Cập nhật vị trí Player liên tục
+            float distance = Vector3.Distance(transform.position, player.position);
+            Debug.Log("Enemy đang đuổi theo Player! Khoảng cách: " + distance);
+        }
+    }
+
+    public void StartChasing()
+    {
+        isChasing = true;
+        Debug.Log("Enemy bắt đầu đuổi theo Player!");
+    }
+}
